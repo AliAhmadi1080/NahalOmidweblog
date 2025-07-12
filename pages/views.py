cf985f7abb936779749f59180d8848d51dacbd17
Ali <aliahmadi.xx90@gmail.comOmit>
2025-07-12 21:29:26 +0330

    refactor all htmls

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from blog.models import Blog, Comment
 from django.conf import settings
 from django.core.mail import send_mail
 # Create your views here.
 
+
 def aboutpage(request):
     return render(request, 'pages/about.html')
+
+
 def contactpage(request):
     if request.method == 'POST':
         if request.POST['email'].strip() != '' and request.POST['text'].strip != '':
@@ -16,32 +19,33 @@ def contactpage(request):
             message = request.POST['text']
             email_from = settings.EMAIL_HOST_USER
             recipient_list = [settings.EMAIL_HOST_USER]
-            send_mail( subject, message, email_from, recipient_list
-                    ,fail_silently=False, auth_user=None
-                    , auth_password=None, connection=None
-                    , html_message=None)
-        
-    return render(request,'pages/contact.html')
+            send_mail(subject, message, email_from, recipient_list, fail_silently=False,
+                      auth_user=None, auth_password=None, connection=None, html_message=None)
+
+    return render(request, 'pages/contact.html')
+
 
 def homepage(request):
     a = Blog.objects.all()[:5]
-    return render(request,'base.html',{'blogs':a})
+    return render(request, 'pages/home.html', {'blogs': a})
 
-def detaleblog(request,pk):
-    a = get_object_or_404(Blog,pk=pk)
-    
+
+def detaleblog(request, pk):
+    a = get_object_or_404(Blog, pk=pk)
+
     if request.method == 'POST':
-        comment = Comment(blog=a,text=request.POST['text'],name=request.POST['name'])
+        comment = Comment(
+            blog=a, text=request.POST['text'], name=request.POST['name'])
         print(comment)
         comment.save()
-            
-    
-    b = Comment.objects.all().filter(blog=a,status='published')
 
-    return render(request,'pages/blogdetaile.html',{'blogdetale':a,'comments':b})
+    b = Comment.objects.all().filter(blog=a, status='published')
+
+    return render(request, 'pages/blogdetaile.html', {'blogdetale': a, 'comments': b})
+
 
 def allblog(request):
     a = Blog.objects.all()
-    context = {'bloglist':a}
+    context = {'bloglist': a}
     print(a, context)
     return render(request, 'pages/bloglist.html', context)
