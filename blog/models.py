c10827ec45c782d9ef5b8bc32bca6a13436e3515
Ali <aliahmadi.xx90@gmail.comOmit>
2025-07-13 20:58:14 +0330

    Add Consultation and Subscriber models, update admin registration, and enhance contact and homepage forms

--- blog/models.py
+++ blog/models.py
@@ -1,51 +1,109 @@
-from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.db import models
+
 
 class Categorys(models.Model):
-    name = models.CharField(max_length=60,verbose_name='اسم عنوان')
+    name = models.CharField(max_length=60, verbose_name='اسم عنوان')
 
     def __str__(self) -> str:
         return self.name
 
 
 class Blog(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='نویسنده')
-    titel  = models.CharField(max_length=60,verbose_name='عنوان')
-    sub_titel = models.CharField(max_length=150,verbose_name='زیر عنوان')
-    image = models.ImageField(verbose_name='عکس',upload_to='statics\assets\img\\',default='statics\assets\img\about-bg.jpg')
-    date = models.DateField(auto_created=True,auto_now_add=True,verbose_name='زمان')
-    categorys = models.ManyToManyField('Categorys',verbose_name='دسته بندی')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='نویسنده')
+    titel = models.CharField(max_length=60, verbose_name='عنوان')
+    sub_titel = models.CharField(max_length=150, verbose_name='زیر عنوان')
+    image = models.ImageField(
+        verbose_name='عکس', upload_to='statics\assets\img\\',
+        default='statics\assets\img\about-bg.jpg')
+    date = models.DateField(
+        auto_created=True, auto_now_add=True, verbose_name='زمان')
+    categorys = models.ManyToManyField('Categorys', verbose_name='دسته بندی')
     text = models.TextField(verbose_name='متن اصلی')
-    like_count = models.PositiveIntegerField(default=0,auto_created=True,verbose_name='تعداد لایک')
-
+    like_count = models.PositiveIntegerField(
+        default=0, auto_created=True, verbose_name='تعداد لایک')
 
     def __str__(self) -> str:
         return f'{self.author} , {self.titel}'
-    
-    def get_absolute_url(self):       
+
+    def get_absolute_url(self):
         return reverse('detaileblog', args=[str(self.pk)])
-    
+
 
 class Comment(models.Model):
     STATUS_CHOICES = (
-        ('checking', 'Checking'),
-        ('rejected','Rejected'),
-        ('published','Published')
+        ('checking', 'درحال بررسی'),
+        ('rejected', 'رد شده'),
+        ('published', 'پذیرفته شده')
     )
     status = models.CharField(max_length=9,
-                  choices=STATUS_CHOICES,default='checking')
-    name = models.CharField(max_length=32)
-    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
-    text = models.TextField()
+                              choices=STATUS_CHOICES,
+                              default='checking', verbose_name='وضعیت')
+    name = models.CharField(max_length=32, verbose_name='نام نویسنده')
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, verbose_name='مطلب')
+    text = models.TextField(verbose_name='محتوا')
 
     def __str__(self) -> str:
         return f'{self.blog.titel}'
-    
+
+
 class Like(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
-    is_liked = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='کاربر')
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, verbose_name='مطلب')
+    is_liked = models.BooleanField(default=False, verbose_name='لایک شده؟')
 
     def __str__(self) -> str:
         return f'{self.user} -> {self.is_liked}'
+
+
+class Consultation(models.Model):
+    STATUS_CHOICES = (
+        ('unreaded', 'خوانده نشده'),
+        ('rejected', 'رد شده'),
+        ('accepted', 'پذیرفته شده')
+    )
+    full_name = models.CharField(
+        max_length=32, verbose_name='نام و نام خانوادگی')
+    phone_number = phone_number = models.CharField(
+        max_length=11,
+        verbose_name='شماره تلفن',
+        validators=[RegexValidator(regex=r'^09\d{9}$',
+                                   message='شماره تلفن میبایست به شکل 09123456789 باشد')])
+    status = models.CharField(max_length=9,
+                              choices=STATUS_CHOICES,
+                              default='unreaded', verbose_name='وضعیت')
+
+    def __str__(self):
+        return f'{self.full_name}, {self.phone_number}'
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+class Subscriber(models.Model):
+    full_name = models.CharField(
+        max_length=32, verbose_name='نام و نام خانوادگی')
+    email = models.CharField(
+        max_length=254,
+        validators=[RegexValidator(
+            regex=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+            message='لطفاً یک آدرس ایمیل معتبر وارد کنید.'
+        )],
+        unique=True,
+        verbose_name='ایمیل'
+    )
+
+    def __str__(self):
+        return f'{self.full_name}, {self.email}'
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        self.email = self.email.replace('.', '')
+        return super().save(*args, **kwargs)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
+from blog.models import Blog, Comment, Consultation, Subscriber
 from django.shortcuts import render, get_object_or_404
 from django.http.request import HttpRequest
 from django.core.mail import send_mail
-from blog.models import Blog, Comment
 from django.conf import settings
 from django.db.models import Q
 # Create your views here.
@@ -13,20 +13,51 @@ def aboutpage(request):
 
 def contactpage(request):
     if request.method == 'POST':
-        if request.POST['email'].strip() != '' and request.POST['text'].strip != '':
-            subject = f'Fatemeh site-contact masege from {request.POST["email"]}'
+        if request.POST['email'].strip() != '' and\
+                request.POST['text'].strip != '':
+            subject = f'Fatemeh site-contact masege from \
+                {request.POST["email"]}'
             message = request.POST['text']
             email_from = settings.EMAIL_HOST_USER
             recipient_list = [settings.EMAIL_HOST_USER]
-            send_mail(subject, message, email_from, recipient_list, fail_silently=False,
-                      auth_user=None, auth_password=None, connection=None, html_message=None)
+            send_mail(subject, message, email_from, recipient_list,
+                      fail_silently=False,
+                      auth_user=None, auth_password=None,
+                      connection=None, html_message=None)
 
     return render(request, 'pages/contact.html')
 
 
-def homepage(request):
-    a = Blog.objects.all()[:5]
-    return render(request, 'pages/home.html', {'blogs': a})
+def homepage(request: HttpRequest):
+    context = {'email':False}
+    if request.method == 'POST':
+        phone_number = request.POST.get('phone_number', False)
+        full_name = request.POST.get('full_name', False)
+        email = request.POST.get('email', False)
+        if phone_number and full_name:
+            try:
+                consultation = Consultation.objects.create(
+                    full_name=full_name, phone_number=phone_number)
+                consultation.save()
+            except:
+                context['result'] = 'شماره تلفن شما درست نیست.'
+                context['error'] = 'true'
+            else:
+                context['result'] = 'شما در لیست قرار گرفتید. با شما تماس گرفته خواهد شد.'
+                context['error'] = 'false'
+        elif email and full_name:
+            context['email'] = True
+            try:
+                Subscriber.objects.create(
+                    full_name=full_name, email=email)
+            except:
+                context['result'] = 'ایمیل واردشده نامعتبر است یا قبلاً در سیستم ثبت شده است.'
+                context['error'] = 'true'
+            else:
+                context['result'] = 'ایمیل شما با موفقیت ثبت شد. از این پس درباره مطالب جدید به شما اطلاع‌رسانی خواهد شد. '
+                context['error'] = 'false'
+
+    return render(request, 'pages/home.html', context=context)
 
 
 def detaleblog(request, pk):
@@ -39,11 +70,13 @@ def detaleblog(request, pk):
 
     b = Comment.objects.all().filter(blog=a, status='published')
 
-    return render(request, 'pages/blogdetaile.html', {'blogdetale': a, 'comments': b})
+    return render(request, 'pages/blogdetaile.html',
+                  {'blogdetale': a, 'comments': b})
 
 
 def allblog(request: HttpRequest):
     parameter = request.GET.get("blogtitle", '')
-    allblogs = Blog.objects.filter(Q(titel__contains=parameter) | Q(sub_titel__contains=parameter))
+    allblogs = Blog.objects.filter(
+        Q(titel__contains=parameter) | Q(sub_titel__contains=parameter))
     context = {'bloglist': allblogs}
     return render(request, 'pages/bloglist.html', context)
